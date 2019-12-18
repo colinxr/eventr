0b3ae6f16a23e690146c0d1a7aa3e44b8996145c
Colin Rabyniuk <colin@madebyarticle.com>
2019-12-18 17:07:16 -0500

    view and edit guest data

--- routes/guest.js
+++ routes/guest.js
@@ -2,6 +2,35 @@ const router    = require('express').Router()
 const Guest     = require('../models/Guest')
 const utils     = require('../utils')
 
+router.put('/:id', utils.isAuthenticated, async (req, res) => {
+  const userData = req.body.user 
+  const { id } = req.params 
+
+  try {
+    Guest.update(userData, {where: { id }})
+   
+    res.writeHead(200, {'Content-Type': 'application/json'})
+    res.end(JSON.stringify({status: 'success', message: 'Guest details updated'}))
+  } catch (error) {
+    utils.errorHandler(error, res)
+  }
+})
+
+router.put('/:id/approve', utils.isAuthenticated, async (req, res) => {
+  const { id } = req.params
+
+  try {
+    Guest.update({ status: 'approved' }, { where: { id } })
+
+    res.writeHead(200, { 'Content-Type': 'application/json' })
+    res.end(JSON.stringify({ status: 'success', message: 'Guest approved' }))
+  } catch (error) {
+    console.log(error)
+    res.writeHead(500, { 'Content-Type': 'application/json' })
+    res.end(JSON.stringify({ status: 'error', message: error.name }))
+  }
+})
+
 router.put('/:id/deny', utils.isAuthenticated, async (req, res) => {
   const { id } = req.params
 
@@ -17,18 +46,14 @@ router.put('/:id/deny', utils.isAuthenticated, async (req, res) => {
   }
 })
 
-router.put('/:id/approve', utils.isAuthenticated, async (req, res) => {c
+router.delete('/:id', utils.isAuthenticated, async (req, res) => {
   const { id } = req.params
-
   try {
-    Guest.update({ status: 'approved' }, { where: { id } })
-
-    res.writeHead(200, { 'Content-Type': 'application/json' })
-    res.end(JSON.stringify({ status: 'success', message: 'Guest approved' }))
+    Guest.destroy({ where: { id }})
+    res.writeHead(200, {'Content-Type': 'application/json'})
+    res.end(JSON.stringify({status: 'success', message: 'User deleted'}))
   } catch (error) {
-    console.log(error)
-    res.writeHead(500, { 'Content-Type': 'application/json' })
-    res.end(JSON.stringify({ status: 'error', message: error.name }))
+    utils.errorHandler(error, res)
   }
 })
 
